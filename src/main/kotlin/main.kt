64e64dc6bc709b6c10bd536ccdb90ddc971eb59e
Nurzhanar <nurzhanar1991@gmail.com>
2024-08-23 11:48:52 +0300

    Changed main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,22 @@
 import kotlin.math.max
 
 fun main() {
-    val dailyLimit = 150_000
-    val monthlyLimit = 600_000
+
     var monthlyAmount = 0
+    var dailyAmount = 0
 
     while (true) {
         println("Введите сумму перевода (в руб.) :")
         val currentAmount = readln().toInt()
-        val transferFee: Double = calculateTransferFee(cardType = "Мир", monthlyAmount = 0, currentAmount)
+        val transferFee: Double = calculateTransferFee(cardType = "Mastercard", monthlyAmount, currentAmount)
         val totalAmount = currentAmount + transferFee.toInt()
         println("Сумма перевода (в руб.) : $currentAmount")
-        monthlyAmount = +currentAmount
-        if (currentAmount > dailyLimit || monthlyAmount > monthlyLimit) {
-            println("Превышен лимит!")
+        dailyAmount += currentAmount
+        monthlyAmount += currentAmount
+        val result = checkLimits(dailyAmount, monthlyAmount)
+
+        if (!result) {
+            println("Лимит превышен!")
             break
         }
         println("Комиссия за перевод составит (в руб.): $transferFee")
@@ -21,14 +24,35 @@ fun main() {
     }
 }
 
+fun checkLimits(dailyAmount: Int, monthlyAmount: Int): Boolean {
+    val dailyLimit = 150_000
+    val monthlyLimit = 600_000
+
+    if (dailyAmount > dailyLimit || monthlyAmount > monthlyLimit) {
+        return false
+    } else return true
+}
+
 fun calculateTransferFee(
     cardType: String = "Мир",
-    monthlyAmount: Int = 0,
+    monthlyAmount: Int,
     currentAmount: Int
 ): Double {
+    val mastercardFreeLimit = 75000
+
     val transferFee = when (cardType) {
         "Visa" -> max(currentAmount * 0.0075, 35.0)
-        "Mastercard" -> (currentAmount - (75000 - monthlyAmount)) * 0.006 + 20
+        "Mastercard" ->
+            if (monthlyAmount < mastercardFreeLimit &&
+                (currentAmount + monthlyAmount) > mastercardFreeLimit
+            ) {
+                (currentAmount - (mastercardFreeLimit - monthlyAmount)) * 0.006 + 20
+            } else if (monthlyAmount > mastercardFreeLimit) {
+                currentAmount * 0.006 + 20
+            } else {
+                0.0
+            }
+
         else -> 0.0
     }
     return transferFee
